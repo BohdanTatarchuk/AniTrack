a6b4432b2cffe17973f8164204ded85dd14ea7e4
Bohdan Tatarchuk <tatarchukbogdan80@gmail.com>
2025-12-02 23:29:27 +0100

    Basic folder structure created

--- app/src/main/java/com/fh/anitrack/ui/MainActivity.java
+++ app/src/main/java/com/fh/anitrack/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.fh.anitrack;
+package com.fh.anitrack.ui;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.fh.anitrack.R;
 
 public class MainActivity extends AppCompatActivity {
 
