566ca4adc80443754de611d931bcd7f37123de18
Bohdan Tatarchuk <tatarchukbogdan80@gmail.com>
2025-12-03 10:59:49 +0100

    conflicts resolved

--- app/src/main/java/com/fh/anitrack/ui/LoginActivity.java
+++ app/src/main/java/com/fh/anitrack/ui/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.fh.anitrack.ui;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +11,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.fh.anitrack.R;
+import com.google.android.material.button.MaterialButton;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private TextView forgotPasswordTextView;
+    private MaterialButton loginButton;
+    private TextView singUpTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        emailEditText = findViewById(R.id.loginEmailEditText);
+        passwordEditText = findViewById(R.id.loginPasswordEditText);
+        forgotPasswordTextView = findViewById(R.id.loginForgotPassword);
+        loginButton = findViewById(R.id.loginButton);
+        singUpTextView = findViewById(R.id.doNotHaveAnAccountText);
     }
 }
