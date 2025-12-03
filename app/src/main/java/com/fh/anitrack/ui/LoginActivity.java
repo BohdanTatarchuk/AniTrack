ad9017f93ebc148c83773b1f1b37d24de16037da
Bohdan Tatarchuk <tatarchukbogdan80@gmail.com>
2025-12-03 10:59:49 +0100

    LoginActivity finished. New values added to resource files

--- app/src/main/java/com/fh/anitrack/ui/LoginActivity.java
+++ app/src/main/java/com/fh/anitrack/ui/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.fh.anitrack.ui;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +11,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.fh.anitrack.R;
+import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private TextView forgotPasswordTextView;
+    private MaterialButton loginButton;
+    private TextView singUpTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +30,11 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        emailEditText = findViewById(R.id.loginEmailEditText);
+        passwordEditText = findViewById(R.id.loginPasswordEditText);
+        forgotPasswordTextView = findViewById(R.id.loginForgotPassword);
+        loginButton = findViewById(R.id.loginButton);
+        singUpTextView = findViewById(R.id.doNotHaveAnAccountText);
     }
 }
